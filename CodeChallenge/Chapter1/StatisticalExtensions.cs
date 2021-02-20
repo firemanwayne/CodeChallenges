cd19a7ff2fe6297732fc9b5fabac77e41415cd66
Wayne Davis <wdavis2002@gmail.com>
2021-02-19 19:26:34 -0600

    Commit

---
+++ CodeChallenge/Chapter1/StatisticalExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace CodeChallenge.Chapter1
+{
+    public static class StatisticalExtensions
+    {
+        public static double TruncatedMean<T>(this IEnumerable<T> values, int discard)
+        {
+            var doubles = values.Select(a => Convert.ToDouble(a))
+                .OrderBy(a => a)
+                .ToArray();
+
+            int minIndex = discard;
+            int maxIndex = doubles.Length - 1 - discard;
+
+            int remaining = maxIndex - minIndex + 1;
+
+            var resultArray = new double[remaining];
+
+            Array.Copy(doubles, minIndex, resultArray, 0, remaining);
+
+            return resultArray.Average();
+        }
+        public static double TruncatedMean<T>(this IEnumerable<T> values, double discard)
+        {
+            var discardInt = (int)(values.Count() * discard);
+
+            return TruncatedMean(values, discardInt);
+        }
+        public static double Median<T>(this IEnumerable<T> values)
+        {
+            var doubles = values               
+                .Select(a => Convert.ToDouble(a))
+                .OrderBy(a => a)
+                .ToArray();
+
+            var valuesCount = doubles.Length;
+
+            if (valuesCount % 2 == 1)
+            {
+                WriteLine($"Total Items: {valuesCount}");
+
+                return doubles[valuesCount / 2];
+            }
+            else
+            {
+                var leftValue = doubles[valuesCount / 2 - 1];
+                var rightValue = doubles[valuesCount / 2];
+
+                WriteLine($"Total Items: {valuesCount}");
+                WriteLine($"Left Value: {leftValue}");
+                WriteLine($"Right Value: {rightValue}");
+
+                return (leftValue + rightValue) / 2.0;
+            }
+        }
+
+        public static double Mode<T>(this IEnumerable<T> values)
+        {
+            var doubles = values
+                .Select(a => Convert.ToDouble(a))
+                .GroupBy(a => a)
+                .Select(a => a)
+                .ToList();                
+
+            foreach(var d in doubles)
+            {
+                
+            }
+        }
+    }
+}
---
+++ CodeChallenge/Program.cs
@@ -0,0 +1,42 @@
+﻿using CodeChallenge.Chapter1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {               
+            var values = SmallList();
+            var largeValues = LargeList();
+
+            var result = values.TruncatedMean(RandomDiscard());
+            Console.WriteLine($"Truncated Int Result: {result}\n\n");
+
+            var doubleResult = values.TruncatedMean(RandomDoubleDiscard());
+            Console.WriteLine($"Truncated Double Result: {doubleResult}\n\n");
+
+            var medianResult = largeValues.Median();
+
+            Console.WriteLine($"Median Result: {medianResult}\n\n");
+
+            Console.ReadKey();
+        }
+
+        static readonly Func<IEnumerable<int>> LargeList = ()
+            => Enumerable.Range(100, 1000)
+                .Select(a => new Random().Next(100, 1000));
+
+        static readonly Func<IEnumerable<int>> SmallList = ()
+            => Enumerable.Range(0, 100)
+                .Select(a => new Random().Next(0, 100));
+
+        static readonly Func<int> RandomDiscard = ()
+            => new Random().Next(1, 5);
+
+        static readonly Func<double> RandomDoubleDiscard = ()
+            => new Random().NextDouble();
+    }
+}
